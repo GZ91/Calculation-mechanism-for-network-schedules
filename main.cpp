88faa7ab804e0fb28dbf5b3c264fe72ba21c8360
Георгий Зырянов <gzyryanov@icloud.com>
2022-08-11 21:46:15 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+#include "nlohmann\json.hpp"
+
+using json = nlohmann::json;
+
+int main()
+{
+	std::ifstream f("example.json");
+	if (f.fail()) return 1; //Если файл не найден
+	json data = json::parse(f);
+	if(data["active"])
+		std::cout << "YES!!!" << std::endl;
+	else std::cout << "NO!!!!" << std::endl;
+	return 0;
+}
